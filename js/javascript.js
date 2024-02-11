881ca26f16a4e593800d79a9dc15f108e352067b
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2024-02-11 16:39:09 -0300

    Desktop layout done

--- js/javascript.js
+++ js/javascript.js
@@ -13,3 +13,35 @@ function updateTime() {
 };
 
 updateTime();
+
+let darkImageList = [
+    'img/backgrounds/dark/dark-img1.jpg'
+]
+
+let lightImageList = [
+    'img/backgrounds/light/light-img1.png'
+]
+
+function imageSelector (list) {
+    window.document.getElementById('imagem').style.backgroundImage = `url("${list[(Math.random() * list.length) | 0] }")`
+}
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    console.log("modo escuro")
+    imageSelector(darkImageList)
+} else {
+    console.log("modo claro")
+    imageSelector(lightImageList)
+}
+
+let musicList = [
+    '2A1w51R7uVAi3l75UT1HTw',
+    '0rnGYYBw32ALUIiuEgwv2c',
+    '3BmGfswCrv2p1It4oCdtU6'
+]
+
+document.addEventListener('DOMContentLoaded', musicChooser());
+
+function musicChooser() {
+    document.getElementById('spotify-iframe').src = `https://open.spotify.com/embed/track/${musicList[(Math.random() * musicList.length) | 0]}`
+}
