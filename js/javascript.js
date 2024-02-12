2ac6f9580da56d69409e80573f0e94fa9e540177
Murilo Monte <105597129+murilomonte@users.noreply.github.com>
2024-02-12 16:50:13 -0300

    footer

--- js/javascript.js
+++ js/javascript.js
@@ -15,7 +15,8 @@ function updateTime() {
 updateTime();
 
 let darkImageList = [
-    'img/backgrounds/dark/dark-img1.jpg'
+    'img/backgrounds/dark/dark-img1.jpg',
+    'img/backgrounds/dark/dark-img2.mp4'
 ]
 
 let lightImageList = [
@@ -44,4 +45,45 @@ document.addEventListener('DOMContentLoaded', musicChooser());
 
 function musicChooser() {
     document.getElementById('spotify-iframe').src = `https://open.spotify.com/embed/track/${musicList[(Math.random() * musicList.length) | 0]}`
+}
+
+let currentCount = 1;
+let lastRun = false;
+let devMessages = [
+    '// -- Iniciando auto destruição da página -- //',
+    'There are no easter eggs up here. Go away.',
+    'Eu já disse, não precisa mais clicar.',
+    'Você é insistente, gosto disso.',
+    'Posso fazer isso o dia todo.',
+    'Never gonna give you up',
+    'Você ainda tá aqui?',
+    'Você é teimoso.',
+    '🐱',
+    ':3'
+];
+
+function devMsgCounter() {
+    if (currentCount > 12 && !lastRun) {
+        document.getElementById('spotify-iframe').src = 'https://open.spotify.com/embed/track/4cOdK2wGLETKBW3PvgPWqT?utm_source=generator';
+        lastRun = true;
+
+    } else if (currentCount > 12) {
+        document.getElementById('popup').innerHTML = devMessages[(Math.floor(Math.random() * devMessages.length))];
+
+    } else if (currentCount > 7) {
+        document.getElementById('popup').innerHTML = 'Não é necessário. Você já é um desenvolvedor.';
+        currentCount++;
+
+    } else if (currentCount == 7) {
+        document.getElementById('popup').innerHTML = 'Você agora é um desenvolvedor!';
+        currentCount++;
+
+    } else if (currentCount >= 3) {
+        document.getElementById('popup').innerHTML = `Faltam ${7 - currentCount} etapas para você se tornar um desenvovedor`;
+        document.getElementById('popup').style.display = 'block';
+        currentCount++;
+
+    } else {
+        currentCount++;
+    };
 }
